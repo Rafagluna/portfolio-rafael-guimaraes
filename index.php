b93b0515d7bfe04bdbd99c957e360242419bcdfe
Rafael <rafagluna@outlook.com>
2024-09-04 22:35:50 -0300

    feat: Criando Herp do site

--- index.php
+++ index.php
@@ -1,11 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rafael Guimarães</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <h1>Portfolio Rafael Guimarães</h1>
+    <div class="content-header">
+        <span class="titulo-header">Rafael Guimarães</span>
+        <ul id="lista-header">
+            <li>Work</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+
+    <!-- HERO -->
+    <div id="hero">
+        <div class="content-title">
+            <div class="title-hero">OLÁ, EU SOU RAFAEL GUIMARÃES</div>
+            <div class="subtitle-hero">
+                Um Desenvolvedor Full Stack apaixonado por criar, desenvolver e crescer através do aprendizado, 
+                com foco em escrever código limpo e de fácil compreensão.
+            </div>
+            <div class="content-buttons">
+                <div class="button-contact">CONTACT ME</div>
+                <div class="button-social">
+                    <img src="./assets/img/linkedin.png" alt="Icon Linkldin">
+                </div>
+                <div class="button-social">
+                    <img src="./assets/img/github.png" alt="Icon GitHub">
+                </div>
+                <!-- <div>iN</div>
+                <div>CONTACT ME</div> -->
+            </div>
+        </div>
+        <div>
+            <img src="./assets/img/foto-perfil.jpg" alt="Foto Perfil" id="foto-perfil">
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
